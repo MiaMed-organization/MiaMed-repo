ff6d885591985765c71ebe3db1e9699ec162c036
Özgenur Yıldız <yildiz.ozgenur@hotmail.com>
2024-08-20 00:00:03 +0300

    booking-success update

--- src/app/components/pages/booking-success/booking-success.component.ts
+++ src/app/components/pages/booking-success/booking-success.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Doctors } from '../../../interfaces/doctors';
+import { DoctorsService } from '../../../services/doctors.service';
+import { Patients } from '../../../interfaces/patients';
+import { PatientsService } from '../../../services/patients.service';
 
 @Component({
   selector: 'app-booking-success',
   templateUrl: './booking-success.component.html',
-  styleUrl: './booking-success.component.css'
+  styleUrl: './booking-success.component.css',
 })
-export class BookingSuccessComponent {
+export class BookingSuccessComponent implements OnInit{
+
+  doctors: Doctors[] = [];
+  firstDoctor!: Doctors;
+
+  patients: Patients[] = [];
+  firstPatient!: Patients;
+
+  totalFee: number = 0;
+
+  constructor(private doctorsService: DoctorsService, private patientsService: PatientsService) {}
+
+  ngOnInit(): void {
+    this.doctorsService.getDoctors().then((data: Doctors[]) => {
+      this.doctors = data;
+      if (this.doctors.length > 0) {
+        this.firstDoctor = this.doctors[0];
+      }
+
+      this.calculateTotalFee();
+    });
+
+    this.patientsService.getPatients().then((data: Patients[]) => {
+      this.patients = data;
+      if (this.patients.length > 0) {
+        this.firstPatient = this.patients[0];
+      }
+
+      this.calculateTotalFee(); 
+    });
+  }
+
+  calculateTotalFee(): void {
+    if (this.firstDoctor && this.firstPatient) {
+      this.totalFee = this.firstDoctor.consultationFee + this.firstPatient.bookingFee + this.firstPatient.consultingFee.price;
+    }
+  }
 
 }
