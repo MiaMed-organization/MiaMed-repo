f3953d3ad2de38bcd48bb7113ae5daa5d539c4f6
Özgenur Yıldız <yildiz.ozgenur@hotmail.com>
2024-09-01 23:26:35 +0300

    update

--- src/app/components/pages/doctors/doctors.component.ts
+++ src/app/components/pages/doctors/doctors.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import { Doctors } from '../../../interfaces/doctors';
 import { DoctorsService } from '../../../services/doctors.service';
+import { ChangeDetectorRef } from '@angular/core';
 
 interface Sort {
-  name: string;
-  code: string;
+  label: string;
+  value: string;
 }
 
 @Component({
@@ -25,29 +26,38 @@ export class DoctorsComponent implements OnInit {
   pageSize: number = 6;
   currentPage: number = 1;
 
-  constructor(private doctorsService: DoctorsService) {}
+  constructor(private doctorsService: DoctorsService, private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     this.doctorsService.getDoctors().then((data) => {
       this.doctors = data;
       this.doctorCount = this.doctorsService.getDoctorCount();
+      this.sortDoctors();
       this.updateDisplayedDoctors();
     });
 
     this.sort = [
-      { name: 'Puan', code: 'PN' },
-      { name: 'Popülerite', code: 'PLR' },
-      { name: 'En son', code: 'ES' }
+      { label: 'Puan', value: 'rating' },
+      { label: 'Popülerite', value: 'recommendation' },
+      { label: 'En son', value: 'default' }
     ];
 
-    this.setPageSizeBasedOnWindowSize(window.innerWidth);
+    this.doctorsService.getDoctors().then((data) => {
+      this.doctors = data;
+      this.doctorCount = this.doctorsService.getDoctorCount();
+      this.updateDisplayedDoctors();
+  });
+
+  this.setPageSizeBasedOnWindowSize(window.innerWidth);
   }
 
   updateDisplayedDoctors(): void {
     const start = (this.currentPage - 1) * this.pageSize;
     const end = start + this.pageSize;
     this.displayedDoctors = this.doctors.slice(start, end);
-  }
+    console.log('Displayed Doctors After Update:', this.displayedDoctors);
+}
+
 
   onPageChange(page: number): void {
     this.currentPage = page;
@@ -66,5 +76,27 @@ export class DoctorsComponent implements OnInit {
 
   activateButton(viewMode: 'grid' | 'list'): void {
     this.viewMode = viewMode;
+  }
+
+  sortDoctors(): void {
+    if (this.sortedBy?.value === 'rating') {
+        this.doctors.sort((a, b) => b.rating - a.rating);
+        console.log('Sorted by Rating:', this.doctors);
+    } else if (this.sortedBy?.value === 'recommendation') {
+        this.doctors.sort((a, b) => b.recommended - a.recommended);
+        console.log('Sorted by Recommendation:', this.doctors);
+    } else if (this.sortedBy?.value === 'default') {
+        this.doctors = [...this.doctorsService.getDoctorsData()];
+        console.log('Sorted by Default:', this.doctors);
+    }
+    this.updateDisplayedDoctors(); 
+    console.log('Displayed Doctors:', this.displayedDoctors);
+    this.cdr.detectChanges(); 
+}
+
+
+  onSortChange(): void {
+    this.sortDoctors(); 
+    console.log('Sort option changed:', this.sortedBy);
   }
 }
